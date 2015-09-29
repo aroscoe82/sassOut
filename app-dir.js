f1b2e0c3916394f5bdfe923060a2199e3a8f24e4
Amanda Roscoe <aroscoe@corpu.com>
2015-09-29 16:53:45 -0400

    implimented server for partials load, modified json structure, left off attempting to impliment conditional templateUrl based on type

--- app-dir.js
+++ app-dir.js
@@ -1,10 +1,52 @@
 'use strict';
 
 angular.module('sandbox.directives', [])
-  .directive('myCustomer', function(){
-    return{
-      restrict: 'A',
+  .directive('sass-text', function() {
+    var baseUrl = 'partials/';
+
+    return {
+      restrict: 'C',
+      replace: false,
       transclude: true,
-      template: 'Name: Someone'
-    };
-  });
+      templateUrl: baseUrl + 'colorPallet.html'
+    } 
+  })
+  .directive('sassType', ['$compile', '$http', '$templateCache', function($compile, $http, $templateCache) {
+    // https://coderwall.com/p/mgtrkg/variable-templates-for-an-angularjs-directive
+        var getTemplate = function(contentType) {
+            var templateLoader,
+            baseUrl = 'partials/',
+            templateMap = {
+                text: 'empty.html',
+                colorPallet: 'colorPallet.html',
+                checkbox: 'checkbox.html',
+            };
+
+            var templateUrl = baseUrl + templateMap[contentType];
+            templateLoader = $http.get(templateUrl, {cache: $templateCache});
+
+            return templateLoader;
+
+        }
+
+        var linker = function(scope, element, attrs) {
+
+            var loader = getTemplate(scope.post.type);
+
+            var promise = loader.success(function(html) {
+                element.html(html);
+            }).then(function (response) {
+                element.replaceWith($compile(element.html())(scope));
+            });
+        }
+
+        return {
+            restrict: 'A',
+            replace: false,
+            transclude: true,
+            scope: {
+                post:'='
+            },
+            link: linker
+        };
+    }]);
--- app.js
+++ app.js
@@ -4,16 +4,73 @@
     .controller('sandboxController', function($scope, $http, test){
       var sandboxCtrl = this;
 
-      var sassInputs = [
-        {type: 'text', label: 'Brand Primary', name: 'brand_prime', value: '#1b4cab'},
-        {type: 'text', label: 'Brand Secondary',name: 'brand_second', value: '#'+shadeColor1('1b4cab', 25.5)}, //#628fe6
-        {type: 'text', label: 'Body Background Color',name: 'bg_color', value: '#d8d8d8'},
-        {type: 'text', label: 'Body Text Color',name: 'text_color', value: '#000'},
-        {type: 'text', label: 'Panel Background Color',name: 'dropdownpanel_bg_color', value: '#eee'},
-        {type: 'text', label: 'Panel Text Color',name: 'dropdownpanel_text_color', value: '#000'},
-        {type: 'text', label: 'Link Color',name: 'link_color', value: '#000'},
-        {type: 'text', label: 'Link Hover Color',name: 'link_hover_color', value: '#000'},
-      ];
+      // var sassInputs = [
+      //   {type: 'text', label: 'Brand Primary', name: 'brand_prime', value: '#1b4cab'},
+      //   {type: 'text', label: 'Brand Secondary',name: 'brand_second', value: '#'+lightenColor('1b4cab', 25.5)}, //#628fe6
+      //   {type: 'text', label: 'Body Background Color',name: 'bg_color', value: '#d8d8d8'},
+      //   {type: 'text', label: 'Body Text Color',name: 'text_color', value: '#000'},
+      //   {type: 'text', label: 'Panel Background Color',name: 'dropdownpanel_bg_color', value: '#eee'},
+      //   {type: 'text', label: 'Panel Text Color',name: 'dropdownpanel_text_color', value: '#000'},
+      //   {type: 'text', label: 'Link Color',name: 'link_color', value: '#ff0000'},
+      //   {type: 'text', label: 'Link Hover Color',name: 'link_hover_color', value: '#'+darkenColor('ff0000', 25.5)},
+      // ];
+
+      var sassInputs = {
+        brand_prime: {
+          name: 'brand_prime',
+          type: 'colorPallet', 
+          label: 'Brand Primary',
+          value: '#1b4cab'
+        },
+        brand_secondary: {
+          name: 'brand_secondary',
+          type: 'colorPallet', 
+          label: 'Brand Secondary',
+          value: '#'+lightenColor('1b4cab', 25.5)
+        },
+        bg_color: {
+          name: 'bg_color',
+          type: 'colorPallet', 
+          label: 'Body Background Color',
+          value: '#d8d8d8'
+        },
+        text_color: {
+          name: 'text_color',
+          type: 'colorPallet', 
+          label: 'Body Text Color',
+          value: '#000'
+        },
+        dropdownpanel_bg_color: {
+          name: 'dropdownpanel_bg_color',
+          type: 'colorPallet', 
+          label: 'Panel Background Color',
+          value: '#eee'
+        },
+        dropdownpanel_text_color: {
+          name: 'dropdownpanel_text_color',
+          type: 'colorPallet', 
+          label: 'Panel Text Color',
+          value: '#000'
+        },
+        link_color: {
+          name: 'link_color',
+          type: 'colorPallet', 
+          label: 'Link Color',
+          value: '#ff0000'
+        },
+        link_hover_color: {
+          name: 'link_hover_color',
+          type: 'colorPallet', 
+          label: 'Link Hover Color',
+          value: '#'+darkenColor('ff0000', 25.5)
+        },
+        link_decoration: {
+          name: 'link_decoration',
+          type: 'checkbox', 
+          label: 'Link Text Decoration',
+          value: 'underline'
+        },
+      };
 
       $scope.formData = {};
       $scope.formData.sassInputs = sassInputs;
@@ -26,11 +83,20 @@
     });
 })();
 
-function shadeColor1(color, percent) {  
+function lightenColor(color, percent) {  
   var num = parseInt(color,16),
   amt = Math.round(2.55 * percent),
   R = (num >> 16) + amt,
   G = (num >> 8 & 0x00FF) + amt,
   B = (num & 0x0000FF) + amt;
+  return (0x1000000 + (R<255?R<1?0:R:255)*0x10000 + (G<255?G<1?0:G:255)*0x100 + (B<255?B<1?0:B:255)).toString(16).slice(1);
+}
+
+function darkenColor(color, percent) {  
+  var num = parseInt(color,16),
+  amt = Math.round(2.55 * percent),
+  R = (num >> 16) - amt,
+  G = (num >> 8 & 0x00FF) - amt,
+  B = (num & 0x0000FF) - amt;
   return (0x1000000 + (R<255?R<1?0:R:255)*0x10000 + (G<255?G<1?0:G:255)*0x100 + (B<255?B<1?0:B:255)).toString(16).slice(1);
 }
